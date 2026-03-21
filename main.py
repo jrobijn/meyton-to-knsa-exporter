103c7b233fb6ee692a6dd2e759d4c9d0694da056
Jonathan Robijn <jonathan.robijn1@ingka.ikea.com>
2026-03-21 11:24:02 +0100

    refactor: add dataclass for query output

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import argparse
 import csv
 import os
 import sys
-from dataclasses import dataclass
 from datetime import date
 from pathlib import Path
 
+from model import Result
 from queries import RESULTS_QUERY
 
 load_dotenv()
@@ -46,20 +46,19 @@ def save_tournament_results_for_day(day: date):
     date_str = day.strftime(DATE_FORMAT)
 
     cur.execute(RESULTS_QUERY, (date_str + "%",))
-    rows: list[tuple] = cur.fetchall()
+    rows = [Result(*row) for row in cur.fetchall()]
 
-    rows_per_discipline: dict[str, list[tuple]] = {}
-    for row in rows:
-        discipline = row[9]
-        if discipline not in rows_per_discipline:
-            rows_per_discipline[discipline] = []
-        rows_per_discipline[discipline].append(row)
+    rows_per_discipline: dict[str, list[Result]] = {}
+    for r in rows:
+        if r.discipline not in rows_per_discipline:
+            rows_per_discipline[r.discipline] = []
+        rows_per_discipline[r.discipline].append(r)
 
     OUTPUT_FOLDER.mkdir(exist_ok=True)
 
     for discipline, rows in rows_per_discipline.items():
         # Sort by total, and then by inner tens
-        rows.sort(key=lambda x: (x[15], x[14]), reverse=True)
+        rows.sort(key=lambda r: (r.total, r.inner_tens), reverse=True)
         with open(OUTPUT_FOLDER / f'results-{date_str}-{discipline}.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(
@@ -83,42 +82,24 @@ def save_tournament_results_for_day(day: date):
                 ]
             )
 
-            for i, (
-                sport_pass_id,
-                first_name,
-                last_name,
-                club,
-                club_id,
-                starter_list,
-                starter_list_id,
-                start_number,
-                target_number,
-                discipline,
-                ranking,
-                series1,
-                series2,
-                series3,
-                inner_tens,
-                total
-            ) in enumerate(rows):
-                name = f"{last_name} - {first_name}"
+            for i, row in enumerate(rows):
                 row = [
                     i+1,
-                    discipline,
+                    row.discipline,
                     "",
                     "",
-                    sport_pass_id,
-                    name,
-                    club_id,
-                    club,
-                    series1,
-                    series2,
-                    series3,
+                    row.sport_pass_id,
+                    row.full_name,
+                    row.club_id,
+                    row.club,
+                    row.series1,
+                    row.series2,
+                    row.series3,
                     "",
                     "",
                     "",
-                    inner_tens,
-                    round(total/10)
+                    row.inner_tens,
+                    row.rounded_total,
                 ]
                 writer.writerow(row)
 
---
+++ model.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Result:
+    sport_pass_id: str
+    first_name: str
+    last_name: str
+    club: str
+    club_id: str
+    starter_list: str
+    starter_list_id: str
+    start_number: str
+    target_number: str
+    discipline: str
+    ranking: str
+    series1: int
+    series2: int
+    series3: int
+    inner_tens: int
+    total: int
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.last_name} - {self.first_name}"
+
+    @property
+    def rounded_total(self) -> int:
+        return round(self.total/10)
